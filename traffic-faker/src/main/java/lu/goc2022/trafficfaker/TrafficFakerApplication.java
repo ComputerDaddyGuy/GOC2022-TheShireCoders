b4af292f188df4079cabfa4cd38636a401be3a59
itogui <itogui@MacBook-Pro-de-itogui.local>
2022-06-29 00:26:08 +0200

    init projet traffic-faker

---
+++ traffic-faker/src/main/java/lu/goc2022/trafficfaker/TrafficFakerApplication.java
@@ -0,0 +1,13 @@
+package lu.goc2022.trafficfaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficFakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficFakerApplication.class, args);
+	}
+
+}
---
+++ traffic-faker/src/test/java/lu/goc2022/trafficfaker/TrafficFakerApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.goc2022.trafficfaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficFakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
