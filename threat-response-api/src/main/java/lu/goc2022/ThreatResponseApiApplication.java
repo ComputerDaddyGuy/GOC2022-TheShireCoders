ab322b0ed7751070521a92442c59f733c8f2868b
ito-gui <ilyass.togui@gmail.com>
2022-06-30 00:18:50 +0200

    H2 DB init

--- threat-response-api/src/main/java/lu/goc2022/ThreatResponseApiApplication.java
+++ threat-response-api/src/main/java/lu/goc2022/ThreatResponseApiApplication.java
@@ -1,7 +1,9 @@
 package lu.goc2022;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ThreatResponseApiApplication {
 
 	
